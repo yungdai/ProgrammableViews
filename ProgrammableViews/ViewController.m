de56f274dfc6ee606ae529f44822dc5401bac590
Yung Dai <yungchidai@gmail.com>
2015-04-16 11:01:39 -0400

    Creating a button from code

--- ProgrammableViews/ViewController.m
+++ ProgrammableViews/ViewController.m
@@ -18,12 +18,29 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor blueColor];
+    
+    // creating a button
+    UIButton *buttonOne = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    // button is located at x = 100, y = 100, width = 100, height = 44
+    buttonOne.frame = CGRectMake(100, 100, 100, 44);
+    [buttonOne setTitle:@"Click Me" forState:UIControlStateNormal];
+
+    
+    
+    
 }
 
 - (void)loadView {
+    // creating the view controller ont the fly
     CGRect viewRect = [[UIScreen mainScreen]bounds];
     UIView *view = [[UIView alloc]initWithFrame:viewRect];
     self.view = view;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -31,9 +48,6 @@
     NSLog(@"Touched From View Controller");
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
